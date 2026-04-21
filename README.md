995c5725a9d257885bbae229cc65bffb60d46490
Christian Schrötter <cs@fnx.li>
2026-04-21 23:40:48 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ec-research
+EC RAM dumps from ThinkPads and various shell scripts.
---
+++ dump/lenovo/dump.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#shellcheck disable=SC2024
+
+status()
+{
+	printf '%s\n' "$@"
+}
+
+error()
+{
+	printf '%s: %s\n' "$(basename "$0")" "$*" >&2
+	exit 1
+}
+
+cleanup()
+{
+	#shellcheck disable=SC2317
+	sudo ectool -w 0x81 -z 0x00
+}
+
+#[[ "$EUID" != "0" ]] && error "Missing root privileges!"
+#command -v ectool &>/dev/null || error "ectool not found!"
+sudo bash -c 'command -v ectool' &>/dev/null || error "ectool not found!"
+
+sysfs=()
+sysfs+=(/sys/class/dmi/id/bios_*)
+sysfs+=(/sys/class/dmi/id/board_{name,vendor})
+sysfs+=(/sys/class/dmi/id/product_{family,name})
+sysfs+=(/sys/class/power_supply/*/*)
+
+status "Gathering sysfs data: ${#sysfs[@]} file(s)"
+grep -aH . -- "${sysfs[@]}" 2>/dev/null >"sysfs.txt"
+status
+
+trap cleanup EXIT
+for page in {16..0}
+do
+	[[ "$page" -gt 0 ]] && filter="^a0:" || filter="."
+	hex=$(printf '0x%02x' "$page")
+	filename="$hex.txt"
+
+	status "Next EC RAM page: $hex"
+	sudo ectool -w 0x81 -z "$hex" >"$filename"
+	sudo ectool -d | grep -i -- "$filter" >>"$filename" \
+	&& status "EC RAM page $hex dumped."
+done
