fcd3e0ccd87162605962dce53552ff6c7596721e
Christian Schrötter <cs@fnx.li>
2026-04-22 18:56:33 +0200

    Update dump script

--- dump/lenovo/thinkpad-ec-dump.sh
+++ dump/lenovo/thinkpad-ec-dump.sh
@@ -15,13 +15,21 @@ error()
 cleanup()
 {
 	#shellcheck disable=SC2317
-	sudo ectool -w 0x81 -z 0x00
-	sudo modprobe thinkpad_acpi
+	ectool -w 0x81 -z 0x00
+	#modprobe thinkpad_acpi
 }
 
-#[[ "$EUID" != "0" ]] && error "Missing root privileges!"
-#command -v ectool &>/dev/null || error "ectool not found!"
-sudo bash -c 'command -v ectool' &>/dev/null || error "ectool not found!"
+[[ "$EUID" != "0" ]] && error "Missing root privileges!"
+command -v ectool &>/dev/null || error "ectool not found!"
+
+# TODO: --full option for unfiltered page 0x00?
+# ...
+
+# TODO: Confirmation prompt?
+# ...
+
+# TODO: mktemp?
+# ...
 
 sysfs=()
 sysfs+=(/sys/class/dmi/id/bios_*)
@@ -34,7 +42,7 @@ grep -aH . -- "${sysfs[@]}" 2>/dev/null >"sysfs.txt"
 status
 
 trap cleanup EXIT
-sudo rmmod thinkpad_acpi battery
+rmmod thinkpad_acpi battery
 for page in {79..0}
 do
 	[[ "$page" -gt 0 ]] && filter="^(80|a0):" || filter="."
@@ -42,7 +50,15 @@ do
 	filename="$hex.txt"
 
 	status "Next EC RAM page: $hex"
-	sudo ectool -w 0x81 -z "$hex" >"$filename"
-	sudo ectool -d | grep -Ei -- "$filter" >>"$filename" \
+	ectool -w 0x81 -z "$hex" >"$filename"
+	ectool -d | grep -Ei -- "$filter" >>"$filename" \
 	&& status "EC RAM page $hex dumped."
 done
+cleanup
+trap - EXIT
+
+# TODO: Create TAR file?
+# ...
+
+# TODO: Delete tmpdir!
+# ...
